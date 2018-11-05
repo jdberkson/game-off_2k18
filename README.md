ad175daae7517b3a738e3d8d1819d4a05a5e8254
jdberkson <joelsteraz@email.arizona.edu>
2018-11-04 21:57:53 -0700

    Attempting to draw sheep in random spots

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # game-off_2k18
 Game for the Game Off 2018
 Beginning November 1st
+Testing commit
--- main.lua
+++ main.lua
@@ -1,1 +1,32 @@
-yep
+debug = true
+sheepImg = nil
+num  = 6
+herd = {}
+
+
+function love.load(arg)
+  gamestart = true
+  sheepstanding = love.graphics.newImage('Images/sheepstanding.png')
+
+end
+
+function love.update(dt)
+
+end
+
+function love.draw(dt)
+  if gamestart == true then
+    drawsheep(num)
+  end
+end
+function drawsheep(num)
+  for i = 1,num,1
+  do
+    newsheep = {x = math.random(50, 910), y = math.random(50, 590), speed = 0, mass = 1, img = sheepstanding}
+    table.insert(herd,newsheep)
+  end
+  for i, sheep in ipairs(herd) do
+  	love.graphics.draw(sheep.img, sheep.x, sheep.y)
+  end
+  gamestart = false
+end
